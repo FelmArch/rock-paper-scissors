78201e01833630b708e1389c062e5b94349d83ee
FelmArch <ferrlinda.dl@gmail.com>
2026-04-28 17:28:36 +0200

    feat: adiciona a funcao getComputerChoice

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ console.log("Hello FelmArch")
 
 
 // vamos ao projeto
+
+function getComputerChoice() {
+    const number = Math.floor(Math.random() * 3)
+
+    if (number === 0) {
+        return "rock"
+    } else if (number === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
