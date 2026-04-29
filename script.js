6c684cfeee7953798551c338fc4a3c43043f01e2
FelmArch <ferrlinda.dl@gmail.com>
2026-04-29 13:20:19 +0200

    feat: adicionar funcao playRound

--- script.js
+++ script.js
@@ -7,15 +7,36 @@ function getComputerChoice() {
     const number = Math.floor(Math.random() * 3)
 
     if (number === 0) {
-        return "rock"
+        return 'rock'
     } else if (number === 1) {
-        return "paper"
+        return 'paper'
     } else {
-        return "scissors"
+        return 'scissors'
     }
 }
 
 function getHumanChoice() {
-   const escolha = prompt("Escolhe: rock, paper ou scissors");
+   const escolha = prompt('Escolhe: rock, paper ou scissors');
    return escolha.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('Empate! Ambos escolheram ' + humanChoice);
+    }
+    else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        console.log('Ganhaste! ' + humanChoice + 'vence' + computerChoice);
+    } else {
+        computerScore++;
+        console.log('Perdeste! ' + computerChoice + 'vence' + humanChoice);
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
